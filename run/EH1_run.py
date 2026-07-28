18546a6da82f69f8d72053a1a84488747ea82509
1605136 <1605136@uab.cat>
2026-07-28 15:05:02 +0200

    Update ROOT path to use __file__ for resolution

--- run/EH1_run.py
+++ run/EH1_run.py
@@ -17,7 +17,7 @@ import pyomo.environ as pyo
 from pathlib import Path
 import sys
 
-ROOT = Path(r"C:\Users\Miriam Ucendo\Documents\UNI\5\TFG_mates\1605136_BSc_Math\run\EH1_run.py").resolve().parents[1]
+ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(ROOT))
 
 # Import
